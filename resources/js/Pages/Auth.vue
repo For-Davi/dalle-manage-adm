f2ad581d278578dc90a146ea68496692f1a59d46
Carlos Davi <carlosdavi004@gmail.com>
2025-08-29 11:35:24 -0300

    Feat: Configuração do Inertia

---
+++ resources/js/Pages/Auth.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Bem-vindo ao Dalle Manage Adm!</h1>
+    <p>Projeto configurado com Inertia e Vue 3.</p>
+  </div>
+</template>
+
+<script setup>
+// Exemplo de script
+</script>
